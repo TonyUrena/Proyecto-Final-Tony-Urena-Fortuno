affcb06783a98daa983d83f98668924f505a8283
Tony Ureña <105971074+TonyUrena@users.noreply.github.com>
2024-05-20 20:09:59 +0200

    Añadidas animaciones de entrada en ProfileScreen

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -9,6 +9,20 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  Offset _offset = Offset(0, 1); // Comienza fuera de la pantalla a la derecha
+
+  @override
+  void initState() {
+    super.initState();
+    // Iniciar la animación después de que el widget se haya construido
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _offset =
+            Offset.zero; // Mover a la posición final (dentro de la pantalla)
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +35,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             height: double.infinity,
             width: double.infinity,
           ),
-           Padding(
+          Padding(
             padding: EdgeInsets.all(15),
             child: Column(
               children: [
@@ -29,17 +43,37 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   padding: EdgeInsets.symmetric(vertical: 20),
                   child: Column(
                     children: [
-                      Hero(tag: "heroAvatar", child: CircleAvatar(radius: 55, backgroundImage: AssetImage('assets/avatar.png'))),
-                      Hero(tag: "heroPoints", child: Text("2500", style: Theme.of(context).textTheme.titleLarge,)),
-                      SizedBox(height: 15,),
-                      Text("Francisco Ramírez Pedregal", style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center,),
+                      Hero(
+                          tag: "heroAvatar",
+                          child: CircleAvatar(
+                              radius: 55,
+                              backgroundImage:
+                                  AssetImage('assets/avatar.png'))),
+                      Hero(
+                          tag: "heroPoints",
+                          child: Text(
+                            "2500",
+                            style: Theme.of(context).textTheme.titleLarge,
+                          )),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Francisco Ramírez Pedregal",
+                        style: Theme.of(context).textTheme.headlineMedium,
+                        textAlign: TextAlign.center,
+                      ),
                     ],
                   ),
                 ),
                 Expanded(
                   child: SizedBox(
                     width: double.infinity,
-                    child: Card(
+                    child: AnimatedSlide(
+                      curve: Curves.easeInOutQuad,
+                      duration: Duration(milliseconds: 500),
+                      offset: _offset,
+                      child: Card(),
                     ),
                   ),
                 ),
